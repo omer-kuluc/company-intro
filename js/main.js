e831f502d58b0208c05574af46cf9c85f233b599
omer-kuluc <omerkuluc96@gmail.com>
2025-12-07 21:27:39 +0300

    logo düzenlendi

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 gsap.set(".effect", { autoAlpha: 1 })
 let tl = gsap.timeline()
   .from("h1", { scale: 0.01, duration: 4, ease: "power4.inOut" })
-  .to(".blendImage, .bg", { scale: 0.6, duration: 3.5 }, 0)
+  .to(".blendImage, .bg", { scale: 1, duration: 3.5 }, 0)
   .to(".dark", { opacity: 0, duration: 2 }, ">-=100%")
