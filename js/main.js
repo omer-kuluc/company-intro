ec0994d85e89dc3511b3253db6ab731487228885
omer-kuluc <omerkuluc96@gmail.com>
2025-12-07 23:33:48 +0300

    logo animasyonu düzenlendi

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-gsap.to(".intro-text", { text: "SKY&SEA STUDIO", ease: "power2.inOut", opacity: 1, duration: 3.5, repeat: 1, yoyo: true }, ">");
+gsap.to(".intro-text", { text: "SKY&SEA STUDIO", ease: "power2.inOut", opacity: 1, duration: 3, ease: "power3.inOut" });
 
 
 gsap.set(".effect", { autoAlpha: 1 })
 let tl = gsap.timeline()
-  .from("h1", { scale: 0.01, duration: 4, ease: "power4.inOut" }, "+=3")
-  .to(".blendImage, .bg", { scale: 1, duration: 3.5 },)
+  .from("h1", { scale: 0.01, duration: 3.5, ease: "power4.inOut" }, "+=2.5")
+  .to(".blendImage, .bg", { scale: 1, duration: 2 }, "-=1")
   .to(".dark", { opacity: 0, duration: 2 }, ">-=100%")
